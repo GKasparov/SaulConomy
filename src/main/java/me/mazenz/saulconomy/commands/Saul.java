c68d2442593546ad299efc4fb57daedfed698c0e
Mazen Zahr <alphoramc@gmail.com>
2021-02-11 16:54:03 +1100

    Added /saul reload

---
+++ src/main/java/me/mazenz/saulconomy/commands/Saul.java
@@ -0,0 +1,48 @@
+package me.mazenz.saulconomy.commands;
+
+import me.mazenz.saulconomy.SaulConomy;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Saul implements CommandExecutor {
+
+    private final SaulConomy plugin;
+
+    public Saul(SaulConomy plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (args.length == 1) {
+
+            if (args[0].equalsIgnoreCase("reload")) {
+                if (sender instanceof Player) {
+                    Player p = (Player) sender;
+
+                    if (p.hasPermission("saul.reload")) {
+                        plugin.reloadConfig();
+                        p.sendMessage(ChatColor.RED + "[SaulConomy] Reloaded Configurations");
+                    } else {
+                        p.sendMessage(ChatColor.RED + "Insufficient Permissions");
+                    }
+                } else {
+                    plugin.reloadConfig();
+                    sender.sendMessage(ChatColor.RED + "[SaulConomy] Reloaded Configurations");
+                }
+            }
+        }
+        if (args.length == 0) {
+            sender.sendMessage(ChatColor.YELLOW + "-- [SaulConomy] Current available commands --");
+            sender.sendMessage(ChatColor.YELLOW + "/saul : Check available SaulConomy commands [Aliases]: /saulconomy");
+            sender.sendMessage(ChatColor.YELLOW + "/pay : Send  money to other players");
+            sender.sendMessage(ChatColor.YELLOW + "/bal : Check your account balance [Aliases]: /balance");
+            sender.sendMessage(ChatColor.YELLOW + "/setbal : Set a user's balance [Aliases]: /setbalance, /setmoney");
+        }
+
+        return true;
+    }
+}
